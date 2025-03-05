e7a68657061608f1de04bb0cf8112ec0301bc267
Kabidoye-17 <23365412@studentmail.ul.ie>
2025-03-05 18:48:47 +0000

    ISOOO

--- backend/models/measurement.py
+++ backend/models/measurement.py
@@ -22,6 +22,6 @@ class Measurement:
             'value': float(self.value),
             'type': str(self.type),
             'unit': str(self.unit),
-            'timestamp_utc': self.timestamp_utc,
+            'timestamp_utc': self.timestamp_utc.isoformat(),
             'utc_offset': self.utc_offset
         }
